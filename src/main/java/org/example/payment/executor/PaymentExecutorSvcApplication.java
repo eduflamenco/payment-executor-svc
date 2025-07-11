ae471aa54fb13c5f7601dc8756e80005d8a9398d
edflamenco <eduardo.flamenco15@hotmail.com>
2025-07-11 17:10:43 -0600

    Initial commit

---
+++ src/main/java/org/example/payment/executor/PaymentExecutorSvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.payment.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentExecutorSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentExecutorSvcApplication.class, args);
+    }
+
+}
